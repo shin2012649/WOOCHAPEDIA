1612a5e8c6f024fa59d063baf61f738fcd3e71bd
shin2012649 <shin2012649@gmail.com>
2023-09-12 14:18:03 +0900

    WebMvcConfig 수정

--- src/main/java/com/movie/reviewsite/config/WebMvcConfig.java
+++ src/main/java/com/movie/reviewsite/config/WebMvcConfig.java
@@ -5,12 +5,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-// todo: 영우님 개인 프로젝트에서는 springSecurity 와 함께 구현되어서 몰랐을 수도 있음
-// 이미지를 출력할 때는 spring 에게 해당 경로를 허락해줘야 함
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
 
-    // todo: 허락해줄 경로를 설정해줌. 앞에 file:/// 를 붙여줌
     String PermittedPath = "file:///" +System.getProperty("user.dir") + "/src/main/resources/static/";
 
 //    @Value("${uploadPath}")
